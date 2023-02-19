085f415b2fe3d494c48810f3634c62a11a81235c
Doned-CMD <3004158930101@ingenieria.usac.edu.gt>
2023-02-19 16:33:16 -0600

    Se corrigio un bug de suma en el ENDPOINT calculadora
    En el endpoint "localhost:3000/Calculadora", se vio un error que consistia en una suma y se cambio a una multiplicacion.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 app.post('/Calculadora', (req, res) => {
   const num1 = req.body.numero1;
   const num2 = req.body.numero2;
-  const suma = num1+num2
+  const suma = num1*num2
   res.send({suma});
 });
 
